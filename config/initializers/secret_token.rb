7ad84050462158455453d0f7dc279218437bc671
Dave Sexton <davesexton1@googlemail.com>
2013-02-28 08:51:56 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musicbee::Application.config.secret_token = '9aead7bf9ab4b12e69dd452af9c9c232803fd45fb7c6e9684b5bd804fa76975665a25ca230a29de22a6a85fada3aeb71855396ec29b15bc9a84aee2db380e7a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Musicbee::Application.config.session_store :cookie_store, key: '_musicbee_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Musicbee::Application.config.session_store :active_record_store
